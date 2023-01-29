7ebe1077d8c1946878a5eec65ffd55dcfa5cf44c
amin biglary <biglarymohamadamin75@gmail.com>
2023-01-29 13:18:08 +0330

    complete account-app config

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import test
 
-
+app_name = "main_app"
 urlpatterns = [
-    path('test', test),
+    path('home', test, name="home"),
 ]
--- qr_project/settings.py
+++ qr_project/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # main apps
     'main_app',
+    'account',
 
+    # third-party apps
     'qr_code',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,12 +132,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-from qr_code.qrcode import constants
 
-QR_CODE_URL_PROTECTION = {
-    constants.TOKEN_LENGTH: 30,                         # Optional random token length for URL protection. Defaults to 20.
-    constants.SIGNING_KEY: 'my-secret-signing-key',     # Optional signing key for URL token. Uses SECRET_KEY if not defined.
-    constants.SIGNING_SALT: 'my-signing-salt',          # Optional signing salt for URL token.
-    constants.ALLOWS_EXTERNAL_REQUESTS_FOR_REGISTERED_USER: True  # Tells whether a registered user can request the QR code URLs from outside a site that uses this app. It can be a boolean value used for any user or a callable that takes a user as parameter. Defaults to False (nobody can access the URL without the signature token).
-}
+LOGIN_REDIRECT_URL = "main_app:home"
+LOGOUT_REDIRECT_URL = "account:login"
+LOGIN_URL = "account:login"
