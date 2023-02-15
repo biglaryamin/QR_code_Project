e875bbeca8c54466baa5347e7fddb5ed93296a57
amin biglary <biglarymohamadamin75@gmail.com>
2023-02-15 16:22:29 +0330

    add registration template, functions and urls

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import views
 from django.urls import path
-from .views import show_profile
+from .views import show_profile, register_request
+
 
 app_name="account"
 urlpatterns=[
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path("register/", register_request, name="register"),
 
     path('profile', show_profile, name='show_profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login
 
+from .forms import NewUserForm
 
 def show_profile(request):
     username = request.user.username
@@ -8,3 +11,16 @@ def show_profile(request):
     full_name = request.user.get_full_name()
     return render(request, "account/profile.html", {"username":username,
                             "email":email,"full_name":full_name})
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main_app:home")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="registration/register.html", context={"register_form":form})
